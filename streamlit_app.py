0abfdf2495855dbd104c0bd70d2b77b80a15b077
harshitdwivedi95 <55316080+harshitdwivedi95@users.noreply.github.com>
2022-05-31 23:35:54 +0530

    Update streamlit_app.py
    Added first few lines using 
    // streamlit.title('----------')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new healthy dinner')
+
+streamlit.title('Breakfast menu')
+streamlit.title('Omega 3 & blueberry oatmeal')
+streamlit.title('Kale, Spinach and Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Ranged eggs')
+
