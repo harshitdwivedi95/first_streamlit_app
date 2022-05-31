4b589345c04b0f1c93b76aa7470ad15791b1bda2
harshitdwivedi95 <55316080+harshitdwivedi95@users.noreply.github.com>
2022-05-31 23:58:28 +0530

    Update streamlit_app.py
    Now, doing some user interaction (multi select).

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,17 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+
+
+streamlit.title('My mom's new healthy dinner')
+
+streamlit.header('Breakfast favorites')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Ranged eggs')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
+                
 #Let's put a pick list here so they can pick the fruit they want to include
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
