f0e451478cbf7291b9a749e2a486c5c1fb00ccac
harshitdwivedi95 <55316080+harshitdwivedi95@users.noreply.github.com>
2022-06-01 00:16:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import streamlit
 import pandas
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
-#Let's put a pick list here so they can pick the fruit they want to include  -- SELECTING FRUITS NAME
-my_fruit_list = my_fruit_list.set_index('Fruit') 
-
 streamlit.title("My mom's new healthy dinner")
 
 streamlit.header('Breakfast favorites')
@@ -15,6 +9,11 @@ streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Ranged eggs')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#streamlit.dataframe(my_fruit_list)
+#Let's put a pick list here so they can pick the fruit they want to include  -- SELECTING FRUITS NAME
+my_fruit_list = my_fruit_list.set_index('Fruit') 
 
 #Let's put a pick list here so they can pick the fruit they want to include  -- SELECTING INDEX NUMBERS
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
